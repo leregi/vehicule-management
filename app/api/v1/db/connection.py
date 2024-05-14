d16c4ce205313c39432f21314576a2dedc4b6d55
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-05-14 12:40:03 -0400

    add init drop get_async_session features for the DB

--- app/api/v1/db/connection.py
+++ app/api/v1/db/connection.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel.ext.asyncio import AsyncSession
+from sqlmodel import SQLModel
 
 from ..core.settings import settings
 
@@ -9,4 +10,20 @@ async_engine = create_async_engine(
     echo=True,
     future=True
 )
+
+
+async def init_db():
+    async with async_engine.begin() as postgres_conn:
+        await postgres_conn.run_sync(SQLModel.metadata.create_all)
+
+async def drop_db():
+    async with async_engine.begin() as postgres_conn:
+        await postgres_conn.run_sync(SQLModel.metadata.drop_all)
+
+async def get_async_session()->AsyncSession:
+    async_session = sessionmaker(
+       bind=async_engine, class_=AsyncSession, expire_on_commit=False
+   )
+    async with async_session as session:
+        yield session
 
