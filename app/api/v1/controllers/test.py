18138e13ac6b8fc662399892035618f3b54e33fb
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-05-16 16:18:23 -0400

    create a simple people

--- app/api/v1/controllers/test.py
+++ app/api/v1/controllers/test.py
@@ -1,9 +1,25 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from typing import Union
+from sqlmodel.ext.asyncio import AsyncSession
+
+from ..schemas.test_schemas import SimplePeopleCreate
+from ..repository.test_repository import TestRepository
+from ..db import get_async_session
 
 
 test_router = APIRouter()
 
-@test_router.post("/")
-async def add_test():
-    pass
+@test_router.post("/"
+    response_model=SimplePeopleCreate,
+    status_code=status.HTTP_200_OK                  
+)
+async def add_test(simple_people:SimplePeopleCreate, session:AsyncSession = Depends(get_async_session)):
+    simple_people_data = simple_people.model_dump()
+    if not simple_people_data:
+            raise HTTPException(status_code=400, detail="bad request")
+    registered_simple_people = await TestRepository.create_simple_people(data=simple_people_data,session=session)
+    if not registered_simple_people:
+            raise HTTPException(status_code=400, detail="bad request on the DB action")
+    return registered_simple_people
+
+    
