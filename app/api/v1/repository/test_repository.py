99cba4d84cdf044c237624a320bd54ab046da469
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-05-16 16:18:23 -0400

    create a simple people

--- app/api/v1/repository/test_repository.py
+++ app/api/v1/repository/test_repository.py
@@ -1,3 +1,11 @@
+from sqlmodel.ext.asyncio import AsyncSession
+from ..models.test_mod import People
 
-class Test():
-    pass
+
+class TestRepository():
+    @staticmethod
+    async def create_simple_people(session:AsyncSession,data):
+        simple_people = People(fullname=data.fullname,email=data.email)
+        session.add(simple_people)
+        session.commit()
+        return simple_people
--- app/api/v1/schemas/test_schemas.py
+++ app/api/v1/schemas/test_schemas.py
@@ -1,16 +1,24 @@
 from sqlmodel import SQLModel
-from pydantic import EmailStr
+from pydantic import EmailStr, ConfigDict
 
 
 class PeopleBase(SQLModel):
+    model_config = ConfigDict(from_attributes=True)
     fullname:str
     email:EmailStr
 
 class NationalCardBase(SQLModel):
+    model_config = ConfigDict(from_attributes=True)
     cin:str
 
 class DivisionBase(SQLModel):
+    model_config = ConfigDict(from_attributes=True)
     name:str
 
 class TeamBase(SQLModel):
+    model_config = ConfigDict(from_attributes=True)
     name:str
+
+
+class SimplePeopleCreate(PeopleBase):
+    pass
