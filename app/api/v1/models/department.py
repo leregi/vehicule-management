17817adc21802cb9672af06a4fd5702f58c8c4c2
Alexandro Disla <32604741+AD0791@users.noreply.github.com>
2024-05-24 11:35:42 -0400

    init departement and employee

--- app/api/v1/models/department.py
+++ app/api/v1/models/department.py
@@ -5,4 +5,17 @@ from sqlalchemy import func, Column, DateTime
 
 class Departement(SQLModel, table=True):
     id:Optional[int] = Field(default=None,primary_key=True)
-
+    department_name:str = Field(unique=True)
+    created_at:datetime = Field(
+        #default_factory=datetime.now(tz=timezone.utc)
+         sa_column=Column(
+            DateTime(timezone=True), server_default=func.now(), nullable=True
+        )
+    )
+    updated_at:Optional[datetime] = Field(
+        default=None,
+        #sa_column_kwargs={"onupdate": datetime.now(tz=timezone.utc)}
+        sa_column=Column(
+            DateTime(timezone=True), onupdate=func.now(), nullable=True
+        )
+    )
--- app/api/v1/models/employee.py
+++ app/api/v1/models/employee.py
@@ -4,4 +4,21 @@ from typing import Optional,List, Callable
 from sqlalchemy import func, Column, DateTime
 
 class Employee(SQLModel, table=True):
-    pass
+    id:Optional[int] = Field(default=None,primary_key=True)
+    first_name:str
+    last_name:str
+    telephone:str = Field(unique=True)
+    email:str = Field(unique=True)
+    created_at:datetime = Field(
+        #default_factory=datetime.now(tz=timezone.utc)
+         sa_column=Column(
+            DateTime(timezone=True), server_default=func.now(), nullable=True
+        )
+    )
+    updated_at:Optional[datetime] = Field(
+        default=None,
+        #sa_column_kwargs={"onupdate": datetime.now(tz=timezone.utc)}
+        sa_column=Column(
+            DateTime(timezone=True), onupdate=func.now(), nullable=True
+        )
+    )
